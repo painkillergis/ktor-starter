painkillergis/fall-color-history

refactor version btest to use embedded server test listener

src/test/kotlin/com/painkillergis/fall_color_history/version/VersionBTest.kt
@@ -1,20 +1,22 @@
 package com.painkillergis.fall_color_history.version
 
-import com.painkillergis.fall_color_history.util.withConfiguredTestApplication
+import com.painkillergis.fall_color_history.util.EmbeddedServerTestListener
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldMatch
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.testing.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 
 class VersionBTest : FunSpec({
+  listeners(EmbeddedServerTestListener)
+
   test("get /version returns version") {
-    withConfiguredTestApplication {
-      handleRequest(HttpMethod.Get, "/version").apply {
-        response.status() shouldBe HttpStatusCode.OK
-        Json.decodeFromString<Map<String, String>>(response.content!!)["version"] shouldMatch Regex("\\d+")
+    EmbeddedServerTestListener.withEmbeddedServerHttpClient {
+      get<HttpResponse>("/version").apply {
+        status shouldBe HttpStatusCode.OK
+        receive<Map<String, String>>()["version"] shouldMatch Regex("\\d+")
       }
     }
   }

painkillergis/ktor-starter

src/test/kotlin/com/painkillergis/ktor_starter/version/VersionBTest.kt
@@ -1,20 +1,22 @@
 package com.painkillergis.ktor_starter.version
 
-import com.painkillergis.ktor_starter.util.withConfiguredTestApplication
+import com.painkillergis.ktor_starter.util.EmbeddedServerTestListener
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldMatch
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.testing.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 
 class VersionBTest : FunSpec({
+  listeners(EmbeddedServerTestListener)
+
   test("get /version returns version") {
-    withConfiguredTestApplication {
-      handleRequest(HttpMethod.Get, "/version").apply {
-        response.status() shouldBe HttpStatusCode.OK
-        Json.decodeFromString<Map<String, String>>(response.content!!)["version"] shouldMatch Regex("\\d+")
+    EmbeddedServerTestListener.withEmbeddedServerHttpClient {
+      get<HttpResponse>("/version").apply {
+        status shouldBe HttpStatusCode.OK
+        receive<Map<String, String>>()["version"] shouldMatch Regex("\\d+")
       }
     }
   }