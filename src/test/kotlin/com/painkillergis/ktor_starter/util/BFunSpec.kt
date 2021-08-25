painkillergis/fall-color-history

extract BFunSpec and shutdown only after all tests

src/test/kotlin/com/painkillergis/fall_color_history/util/BFunSpec.kt
@@ -1,0 +1,8 @@
+package com.painkillergis.fall_color_history.util
+
+import io.kotest.core.spec.style.FunSpec
+
+abstract class BFunSpec(val body: FunSpec.() -> Unit) : FunSpec({
+  listeners(EmbeddedServerTestListener.ServerStart)
+  body(this)
+})

src/test/kotlin/com/painkillergis/fall_color_history/version/VersionBTest.kt
@@ -1,7 +1,7 @@
 package com.painkillergis.fall_color_history.version
 
-import com.painkillergis.fall_color_history.util.EmbeddedServerTestListener
-import io.kotest.core.spec.style.FunSpec
+import com.painkillergis.fall_color_history.util.BFunSpec
+import com.painkillergis.fall_color_history.util.EmbeddedServerTestListener.withEmbeddedServerHttpClient
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldMatch
 import io.ktor.client.call.*
@@ -9,11 +9,9 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 
-class VersionBTest : FunSpec({
-  listeners(EmbeddedServerTestListener)
-
+class VersionBTest : BFunSpec({
   test("get /version returns version") {
-    EmbeddedServerTestListener.withEmbeddedServerHttpClient {
+    withEmbeddedServerHttpClient {
       get<HttpResponse>("/version").apply {
         status shouldBe HttpStatusCode.OK
         receive<Map<String, String>>()["version"] shouldMatch Regex("\\d+")

painkillergis/ktor-starter

src/test/kotlin/com/painkillergis/ktor_starter/util/BFunSpec.kt
@@ -1,0 +1,8 @@
+package com.painkillergis.ktor_starter.util
+
+import io.kotest.core.spec.style.FunSpec
+
+abstract class BFunSpec(val body: FunSpec.() -> Unit) : FunSpec({
+  listeners(EmbeddedServerTestListener.ServerStart)
+  body(this)
+})

src/test/kotlin/com/painkillergis/ktor_starter/version/VersionBTest.kt
@@ -1,7 +1,7 @@
 package com.painkillergis.ktor_starter.version
 
-import com.painkillergis.ktor_starter.util.EmbeddedServerTestListener
-import io.kotest.core.spec.style.FunSpec
+import com.painkillergis.ktor_starter.util.BFunSpec
+import com.painkillergis.ktor_starter.util.EmbeddedServerTestListener.withEmbeddedServerHttpClient
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldMatch
 import io.ktor.client.call.*
@@ -9,11 +9,9 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 
-class VersionBTest : FunSpec({
-  listeners(EmbeddedServerTestListener)
-
+class VersionBTest : BFunSpec({
   test("get /version returns version") {
-    EmbeddedServerTestListener.withEmbeddedServerHttpClient {
+    withEmbeddedServerHttpClient {
       get<HttpResponse>("/version").apply {
         status shouldBe HttpStatusCode.OK
         receive<Map<String, String>>()["version"] shouldMatch Regex("\\d+")