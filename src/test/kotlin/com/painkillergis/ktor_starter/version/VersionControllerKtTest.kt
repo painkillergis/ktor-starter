19f45ca4e4656b1ee294ed9effa8031a9ee7cf1b
tyler <tyler@cruftbusters.com>
2021-09-13 19:51:51 -0500

    invert controller test

--- src/test/kotlin/com/painkillergis/ktor_starter/version/VersionControllerKtTest.kt
+++ src/test/kotlin/com/painkillergis/ktor_starter/version/VersionControllerKtTest.kt
@@ -1,30 +1,30 @@
 package com.painkillergis.ktor_starter.version
 
+import com.cruftbusters.ktor_baseurl_util.withEmbeddedServer
 import com.painkillergis.ktor_starter.globalModules
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.server.testing.*
 import io.mockk.every
 import io.mockk.mockk
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 
 class VersionControllerKtTest : FunSpec({
   val versionService = mockk<VersionService>()
-
-  test("get /version returns version number") {
-    withTestApplication({
-      globalModules()
-      versionController(
-        versionService,
-      )
-    }) {
+  withEmbeddedServer({
+    globalModules()
+    versionController(
+      versionService,
+    )
+  }) { httpClient ->
+    test("get /version returns version number") {
       every { versionService.get() } returns "the version"
 
-      handleRequest(HttpMethod.Get, "/version").apply {
-        response.status() shouldBe HttpStatusCode.OK
-        Json.decodeFromString<Map<String, String>>(response.content!!) shouldBe mapOf("version" to "the version")
+      httpClient.get<HttpResponse>("/version").apply {
+        status shouldBe HttpStatusCode.OK
+        receive<Map<String, String>>() shouldBe mapOf("version" to "the version")
       }
     }
   }
